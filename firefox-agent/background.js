6bd02cffd234cf0798543594dc9d5a6cbb14ca32
Samuele Casarin <eleumasc@gmail.com>
2023-11-16 09:31:52 -0500

    Remove global wrapper from firefox-agent scripts

--- firefox-agent/background.js
+++ firefox-agent/background.js
@@ -1,236 +1,234 @@
-(async function () {
-  const asyncDelay = async (timeoutMs) => {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, timeoutMs);
-    });
-  };
+const asyncDelay = async (timeoutMs) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeoutMs);
+  });
+};
 
-  const poll = async (callback, ttl, timeoutMs) => {
-    while (true) {
-      try {
-        return await callback();
-      } catch (e) {
-        ttl -= 1;
-        if (ttl > 0) {
-          await asyncDelay(timeoutMs);
-        } else {
-          throw e;
-        }
-      }
-    }
-  };
-
-  const navigate = async (tabId, url) => {
-    let complete = null;
-    let completeError = null;
-    const willNavigate = new Promise((resolve, reject) => {
-      complete = () => resolve();
-      completeError = (reason) => reject(reason);
-    });
-
-    const STATE_START = 0;
-    const STATE_ERROR = 1;
-    const STATE_COMMITTED = 2;
-    let state = STATE_START;
-    let lastError = null;
-
-    const filterEvents = (listener) => {
-      return (details) => {
-        if (details.tabId === tabId && details.frameId === 0) {
-          listener(details);
-        }
-      };
-    };
-
-    const onBeforeNavigate = filterEvents(() => {
-      state = STATE_START;
-    });
-
-    const onCommitted = filterEvents(() => {
-      switch (state) {
-        case STATE_START:
-          state = STATE_COMMITTED;
-          break;
-        case STATE_ERROR:
-          completeError(lastError);
-          break;
-        default:
-      }
-    });
-
-    const onCompleted = filterEvents(() => {
-      complete();
-    });
-
-    const onErrorOccurred = filterEvents((details) => {
-      state = STATE_ERROR;
-      lastError = details.error;
-    });
-
-    const webNavigation = browser.webNavigation;
-    webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
-    webNavigation.onCommitted.addListener(onCommitted);
-    webNavigation.onCompleted.addListener(onCompleted);
-    webNavigation.onErrorOccurred.addListener(onErrorOccurred);
+const poll = async (callback, ttl, timeoutMs) => {
+  while (true) {
     try {
-      const willUpdate = browser.tabs.update(tabId, { url });
-      await Promise.all([willUpdate, willNavigate]);
-    } finally {
-      webNavigation.onBeforeNavigate.removeListener(onBeforeNavigate);
-      webNavigation.onCommitted.removeListener(onCommitted);
-      webNavigation.onCompleted.removeListener(onCompleted);
-      webNavigation.onErrorOccurred.removeListener(onErrorOccurred);
-    }
-  };
-
-  const useTab = async (callback) => {
-    const { id: tabId } = await browser.tabs.create({});
-    try {
-      return await callback(tabId);
-    } finally {
-      await browser.tabs.remove(tabId);
-    }
-  };
-
-  const useNetworkLogging = async (tabId, callback) => {
-    const state = { requests: [], redirects: [] };
-
-    const filterEvents = (listener) => {
-      return (details) => {
-        if (details.tabId === tabId) {
-          listener(details);
-        }
-      };
-    };
-
-    const onBeforeRequest = filterEvents((details) => {
-      state.requests = [...state.requests, details];
-    });
-
-    const onBeforeRedirect = filterEvents((details) => {
-      state.redirects = [...state.redirects, details];
-    });
-
-    const webRequest = browser.webRequest;
-    const webRequestFilter = { urls: ["*://*/*"] };
-    webRequest.onBeforeRequest.addListener(onBeforeRequest, webRequestFilter);
-    webRequest.onBeforeRedirect.addListener(onBeforeRedirect, webRequestFilter);
-    try {
-      return await callback(state);
-    } finally {
-      webRequest.onBeforeRequest.removeListener(onBeforeRequest);
-      webRequest.onBeforeRedirect.removeListener(onBeforeRedirect);
-    }
-  };
-
-  const runAnalysis = async (url) => {
-    return await useTab(async (tabId) => {
-      return await useNetworkLogging(tabId, async (networkLoggingState) => {
-        try {
-          const resolved = await Promise.race([
-            (async () => (await navigate(tabId, url), true))(),
-            asyncDelay(30000),
-          ]);
-          if (resolved === true) {
-            await asyncDelay(5000);
-          }
-        } catch {
-          throw new Error("Navigation error");
-        }
-
-        const pageData = (
-          await Promise.all(
-            (
-              await browser.webNavigation.getAllFrames({ tabId })
-            ).map(async ({ frameId }) => {
-              try {
-                const result = await browser.tabs.sendMessage(
-                  tabId,
-                  { action: "Snapshot" },
-                  { frameId }
-                );
-                return result;
-              } catch (e) {
-                return null;
-              }
-            })
-          )
-        ).filter((element) => element !== null);
-        const networkData = { ...networkLoggingState };
-        const data = { pageData, networkData };
-
-        return data;
-      });
-    });
-  };
-
-  const dispatchTask = async (command, parameter) => {
-    switch (command) {
-      case "RunAnalysis": {
-        const { url } = parameter;
-        return await runAnalysis(url);
-      }
-      case "Shutdown": {
-        shutdownFlagged = true;
-        return true;
-      }
-    }
-  };
-
-  const acceptTask = async (task) => {
-    const { id: taskId, command, parameter } = task;
-    try {
-      const result = await dispatchTask(command, parameter);
-      return { taskId, status: "success", detail: result };
+      return await callback();
     } catch (e) {
-      return { taskId, status: "failure", detail: { error: String(e) } };
-    }
-  };
-
-  const closeBrowser = async () => {
-    await browser.tabs.remove(
-      (await browser.tabs.query({})).map(({ id: tabId }) => tabId)
-    );
-  };
-
-  const getConnectUrlFromStartTab = async () => {
-    const PREFIX = "firefox-agent:";
-
-    return await poll(
-      async () => {
-        const allTabs = await browser.tabs.query({});
-        const targetTab = allTabs.find((tab) => tab.title.startsWith(PREFIX));
-        if (targetTab) {
-          const connectUrl = targetTab.title.substring(PREFIX.length);
-          return connectUrl;
-        } else {
-          throw new Error("Connect URL not found");
-        }
-      },
-      100,
-      100
-    );
-  };
-
-  let shutdownFlagged = false;
-
-  const main = async () => {
-    const connectUrl = await getConnectUrlFromStartTab();
-
-    const socket = new WebSocket(connectUrl);
-
-    socket.addEventListener("message", async (event) => {
-      const task = JSON.parse(event.data);
-      const taskResult = await acceptTask(task);
-      socket.send(JSON.stringify(taskResult));
-
-      if (shutdownFlagged) {
-        await closeBrowser();
+      ttl -= 1;
+      if (ttl > 0) {
+        await asyncDelay(timeoutMs);
+      } else {
+        throw e;
       }
-    });
+    }
+  }
+};
+
+const navigate = async (tabId, url) => {
+  let complete = null;
+  let completeError = null;
+  const willNavigate = new Promise((resolve, reject) => {
+    complete = () => resolve();
+    completeError = (reason) => reject(reason);
+  });
+
+  const STATE_START = 0;
+  const STATE_ERROR = 1;
+  const STATE_COMMITTED = 2;
+  let state = STATE_START;
+  let lastError = null;
+
+  const filterEvents = (listener) => {
+    return (details) => {
+      if (details.tabId === tabId && details.frameId === 0) {
+        listener(details);
+      }
+    };
   };
 
-  main();
-})();
+  const onBeforeNavigate = filterEvents(() => {
+    state = STATE_START;
+  });
+
+  const onCommitted = filterEvents(() => {
+    switch (state) {
+      case STATE_START:
+        state = STATE_COMMITTED;
+        break;
+      case STATE_ERROR:
+        completeError(lastError);
+        break;
+      default:
+    }
+  });
+
+  const onCompleted = filterEvents(() => {
+    complete();
+  });
+
+  const onErrorOccurred = filterEvents((details) => {
+    state = STATE_ERROR;
+    lastError = details.error;
+  });
+
+  const webNavigation = browser.webNavigation;
+  webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
+  webNavigation.onCommitted.addListener(onCommitted);
+  webNavigation.onCompleted.addListener(onCompleted);
+  webNavigation.onErrorOccurred.addListener(onErrorOccurred);
+  try {
+    const willUpdate = browser.tabs.update(tabId, { url });
+    await Promise.all([willUpdate, willNavigate]);
+  } finally {
+    webNavigation.onBeforeNavigate.removeListener(onBeforeNavigate);
+    webNavigation.onCommitted.removeListener(onCommitted);
+    webNavigation.onCompleted.removeListener(onCompleted);
+    webNavigation.onErrorOccurred.removeListener(onErrorOccurred);
+  }
+};
+
+const useTab = async (callback) => {
+  const { id: tabId } = await browser.tabs.create({});
+  try {
+    return await callback(tabId);
+  } finally {
+    await browser.tabs.remove(tabId);
+  }
+};
+
+const useNetworkLogging = async (tabId, callback) => {
+  const state = { requests: [], redirects: [] };
+
+  const filterEvents = (listener) => {
+    return (details) => {
+      if (details.tabId === tabId) {
+        listener(details);
+      }
+    };
+  };
+
+  const onBeforeRequest = filterEvents((details) => {
+    state.requests = [...state.requests, details];
+  });
+
+  const onBeforeRedirect = filterEvents((details) => {
+    state.redirects = [...state.redirects, details];
+  });
+
+  const webRequest = browser.webRequest;
+  const webRequestFilter = { urls: ["*://*/*"] };
+  webRequest.onBeforeRequest.addListener(onBeforeRequest, webRequestFilter);
+  webRequest.onBeforeRedirect.addListener(onBeforeRedirect, webRequestFilter);
+  try {
+    return await callback(state);
+  } finally {
+    webRequest.onBeforeRequest.removeListener(onBeforeRequest);
+    webRequest.onBeforeRedirect.removeListener(onBeforeRedirect);
+  }
+};
+
+const runAnalysis = async (url) => {
+  return await useTab(async (tabId) => {
+    return await useNetworkLogging(tabId, async (networkLoggingState) => {
+      try {
+        const resolved = await Promise.race([
+          (async () => (await navigate(tabId, url), true))(),
+          asyncDelay(30000),
+        ]);
+        if (resolved === true) {
+          await asyncDelay(5000);
+        }
+      } catch {
+        throw new Error("Navigation error");
+      }
+
+      const pageData = (
+        await Promise.all(
+          (
+            await browser.webNavigation.getAllFrames({ tabId })
+          ).map(async ({ frameId }) => {
+            try {
+              const result = await browser.tabs.sendMessage(
+                tabId,
+                { action: "Snapshot" },
+                { frameId }
+              );
+              return result;
+            } catch (e) {
+              return null;
+            }
+          })
+        )
+      ).filter((element) => element !== null);
+      const networkData = { ...networkLoggingState };
+      const data = { pageData, networkData };
+
+      return data;
+    });
+  });
+};
+
+const dispatchTask = async (command, parameter) => {
+  switch (command) {
+    case "RunAnalysis": {
+      const { url } = parameter;
+      return await runAnalysis(url);
+    }
+    case "Shutdown": {
+      shutdownFlagged = true;
+      return true;
+    }
+  }
+};
+
+const acceptTask = async (task) => {
+  const { id: taskId, command, parameter } = task;
+  try {
+    const result = await dispatchTask(command, parameter);
+    return { taskId, status: "success", detail: result };
+  } catch (e) {
+    return { taskId, status: "failure", detail: { error: String(e) } };
+  }
+};
+
+const closeBrowser = async () => {
+  await browser.tabs.remove(
+    (await browser.tabs.query({})).map(({ id: tabId }) => tabId)
+  );
+};
+
+const getConnectUrlFromStartTab = async () => {
+  const PREFIX = "firefox-agent:";
+
+  return await poll(
+    async () => {
+      const allTabs = await browser.tabs.query({});
+      const targetTab = allTabs.find((tab) => tab.title.startsWith(PREFIX));
+      if (targetTab) {
+        const connectUrl = targetTab.title.substring(PREFIX.length);
+        return connectUrl;
+      } else {
+        throw new Error("Connect URL not found");
+      }
+    },
+    100,
+    100
+  );
+};
+
+let shutdownFlagged = false;
+
+const main = async () => {
+  const connectUrl = await getConnectUrlFromStartTab();
+
+  const socket = new WebSocket(connectUrl);
+
+  socket.addEventListener("message", async (event) => {
+    const task = JSON.parse(event.data);
+    const taskResult = await acceptTask(task);
+    socket.send(JSON.stringify(taskResult));
+
+    if (shutdownFlagged) {
+      await closeBrowser();
+    }
+  });
+};
+
+main();
--- firefox-agent/index.js
+++ firefox-agent/index.js
@@ -1,81 +1,79 @@
-(async function () {
-  const getCookies = () => {
-    let cookies = [];
-    const cookieString = document.cookie;
-    if (!cookieString) {
-      return cookies;
-    }
-    const tokens = cookieString.split("; ");
-    const tokensLength = tokens.length;
-    for (let i = 0; i < tokensLength; i += 1) {
-      const token = tokens[i];
-      const index = token.indexOf("=");
-      const key = token.substring(0, index);
-      const value = token.substring(index + 1);
-      cookies = [...cookies, { key, value }];
-    }
+const getCookies = () => {
+  let cookies = [];
+  const cookieString = document.cookie;
+  if (!cookieString) {
     return cookies;
-  };
+  }
+  const tokens = cookieString.split("; ");
+  const tokensLength = tokens.length;
+  for (let i = 0; i < tokensLength; i += 1) {
+    const token = tokens[i];
+    const index = token.indexOf("=");
+    const key = token.substring(0, index);
+    const value = token.substring(index + 1);
+    cookies = [...cookies, { key, value }];
+  }
+  return cookies;
+};
 
-  const getStorageItems = () => {
-    let items = [];
-    const length = localStorage.length;
-    for (let i = 0; i < length; i += 1) {
-      const key = localStorage.key(i);
-      const value = localStorage.getItem(key);
-      items = [...items, { key, value }];
-    }
-    return items;
-  };
+const getStorageItems = () => {
+  let items = [];
+  const length = localStorage.length;
+  for (let i = 0; i < length; i += 1) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    items = [...items, { key, value }];
+  }
+  return items;
+};
 
-  const cloneSavedFrame = (savedFrame) => {
-    const {
-      asyncCause,
-      asyncParent,
-      column,
-      functionDisplayName,
-      line,
-      parent,
-      source,
-      sourceId,
-    } = savedFrame;
+const cloneSavedFrame = (savedFrame) => {
+  const {
+    asyncCause,
+    asyncParent,
+    column,
+    functionDisplayName,
+    line,
+    parent,
+    source,
+    sourceId,
+  } = savedFrame;
+  return {
+    asyncCause,
+    asyncParent,
+    column,
+    functionDisplayName,
+    line,
+    parent: parent !== null ? cloneSavedFrame(parent) : null,
+    source,
+    sourceId,
+  };
+};
+
+const getTaintReports = () => {
+  return (
+    XPCNativeWrapper(window.wrappedJSObject["$__taintReports"]) || []
+  ).map((taintReport) => {
     return {
-      asyncCause,
-      asyncParent,
-      column,
-      functionDisplayName,
-      line,
-      parent: parent !== null ? cloneSavedFrame(parent) : null,
-      source,
-      sourceId,
+      ...taintReport,
+      stack: cloneSavedFrame(taintReport.stack),
+      taint: taintReport.str.taint,
     };
-  };
-
-  const getTaintReports = () => {
-    return (
-      XPCNativeWrapper(window.wrappedJSObject["$__taintReports"]) || []
-    ).map((taintReport) => {
-      return {
-        ...taintReport,
-        stack: cloneSavedFrame(taintReport.stack),
-        taint: taintReport.str.taint,
-      };
-    });
-  };
-
-  browser.runtime.onMessage.addListener((message, _, sendResponse) => {
-    const { action } = message;
-    switch (action) {
-      case "Snapshot":
-        sendResponse({
-          frameId: browser.runtime.getFrameId(window),
-          url: document.URL,
-          baseUrl: document.baseURI,
-          cookies: getCookies(),
-          storageItems: getStorageItems(),
-          taintReports: getTaintReports(),
-        });
-        return;
-    }
   });
-})();
+};
+
+browser.runtime.onMessage.addListener((message, _, sendResponse) => {
+  const { action } = message;
+  switch (action) {
+    case "Snapshot":
+      sendResponse({
+        frameId: browser.runtime.getFrameId(window),
+        url: document.URL,
+        baseUrl: document.baseURI,
+        cookies: getCookies(),
+        storageItems: getStorageItems(),
+        taintReports: getTaintReports(),
+      });
+      return;
+  }
+});
